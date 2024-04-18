dda076e29ba78d3169a73951b4f93a67bf2c7870
Mauricio <mauriciorojasanchez@gmail.com>
2024-04-18 14:04:43 -0500

    feat: added components and services.

--- src/main.js
+++ src/main.js
@@ -2,4 +2,43 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Add PrimeVue
+import PrimeVue from "primevue/config";
+
+// Add Primevue Components
+import Card from "primevue/card";
+import Button from "primevue/button";
+import SelectButton from "primevue/selectbutton";
+import Sidebar from "primevue/sidebar";
+import Avatar from "primevue/avatar";
+import Menu from "primevue/menu";
+import Menubar from "primevue/menubar";
+import Toolbar from "primevue/toolbar";
+
+
+// Create App
+const app = createApp(App);
+
+// Add Ripple for button's animation
+// Add PrimeVue Plugin
+app.use(PrimeVue, {ripple : true})
+    .component('pv-card', Card)
+    .component('pv-button', Button)
+    .component('pv-select-button',SelectButton)
+    .component('pv-sidebar', Sidebar)
+    .component('pv-avatar', Avatar)
+    .component('pv-menu',Menu)
+    .component('pv-menubar',Menubar)
+    .component('pv-toolbar', Toolbar);
+
+// Add App Design or Theme
+import 'primevue/resources/themes/md-dark-indigo/theme.css'
+
+// Add PrimeIcons
+import 'primeicons/primeicons.css'
+
+// Add App Prime flex library
+import 'primeflex/primeflex.css'
+
+// Mount into App
+app.mount('#app');
---
+++ src/news/components/article-card.component.vue
@@ -0,0 +1,51 @@
+<script>
+import {Article} from "../model/article.entity.js";
+
+export default {
+  name: "article-card",
+
+  props:{
+    article : Article,
+  }
+
+}
+</script>
+
+<template>
+
+  <pv-card class="m-2">
+      <template #header>
+        <img :alt="article.title" :src="article.urlToImage"/>
+      </template>
+      <!-- TITLE OF THE CARD -->
+      <template #title>
+        <p>{{article.title}}</p>
+      </template>
+      <!-- CONTENT OF THE CARD-->
+      <template #content>
+        <p class="flex align-content-start flex-wrap">
+          <span class="flex-align-items-center justify-content-center">
+            <pv-avatar :aria-label="article.source.name" :image="article.source.urlToLogo" shape="circle" />
+          </span>
+
+          <span class="flex align-items-center justify-content-center">
+              {{article.source.name}}
+          </span>
+        </p>
+
+        <p class="flex align-content-start flex-wrap mt-4">
+            {{article.description}}
+        </p>
+      </template>
+
+      <!-- FOOTER OF THE CARD -->
+      <template #footer>
+        <a :href="article.url" target="_blank">Read More</a>
+      </template>
+  </pv-card>
+
+</template>
+
+<style scoped>
+
+</style>
